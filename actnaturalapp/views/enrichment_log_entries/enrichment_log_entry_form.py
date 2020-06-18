25c78ae4a38eef0fdab2d6c6b5a6f57cdf37d854
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-06-18 13:55:36 -0500

    user can edit and delete enrichment log entries

--- actnaturalapp/views/enrichment_log_entries/enrichment_log_entry_form.py
+++ actnaturalapp/views/enrichment_log_entries/enrichment_log_entry_form.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from actnaturalapp.models import Animal, EnrichmentItem, Employee
+from actnaturalapp.models import Animal, EnrichmentItem, Employee, EnrichmentLogEntry
 
 
 @login_required
@@ -17,6 +17,28 @@ def enrichment_log_entry_form(request):
             'employee': employee,
             'animals': animals,
             'enrichment_items': enrichment_items
+        }
+
+        return render(request, template, context)
+
+@login_required
+def enrichment_log_entry_edit_form(request, enrichment_log_entry_id):
+
+    if request.method == 'GET':
+        
+        employee = Employee.objects.get(pk=request.user.employee.id)
+        enrichment_log_entry = EnrichmentLogEntry.objects.get(pk=enrichment_log_entry_id)
+        animals = Animal.objects.filter(team_id=request.user.employee.team_id)
+        enrichment_items = EnrichmentItem.objects.filter(team_id=request.user.employee.team_id)
+
+        enrichment_log_entry.date = str(enrichment_log_entry.date)
+
+        template = 'enrichment_log_entries/enrichment_log_entry_form.html'
+        context = {
+            'employee': employee,
+            'animals': animals,
+            'enrichment_items': enrichment_items,
+            'enrichment_log_entry': enrichment_log_entry
         }
 
         return render(request, template, context)
--- actnaturalapp/views/enrichment_log_entries/enrichment_log_entry_list.py
+++ actnaturalapp/views/enrichment_log_entries/enrichment_log_entry_list.py
@@ -5,7 +5,7 @@ from actnaturalapp.models import EnrichmentLogEntry, Employee
 
 
 @login_required
-def enrichment_log_entry_list(request):
+def enrichment_log_entry_list(request, enrichment_log_entry_id=None):
 
     if request.method == 'GET':
 
@@ -31,12 +31,38 @@ def enrichment_log_entry_list(request):
     elif request.method == 'POST':
         form_data = request.POST
 
-        new_enrichment_log_entry = EnrichmentLogEntry.objects.create(
-            employee_id = request.user.employee.id,
-            animal_id = form_data['animal'],
-            enrichment_item_id = form_data['enrichment_item'],
-            date = form_data['date'],
-            note = form_data['note']
-        )
+        if (
+            "actual_method" in form_data
+        ):
 
-        return redirect(reverse('actnaturalapp:enrichment_log_entries'))
+            enrichment_log_entry = EnrichmentLogEntry.objects.get(pk=enrichment_log_entry_id)
+
+            if (form_data["actual_method"] == "DELETE"):
+
+                enrichment_log_entry.delete()
+
+                return redirect(reverse('actnaturalapp:enrichment_log_entries'))
+
+            elif (form_data["actual_method"] == "PUT"):
+
+                enrichment_log_entry.employee_id = request.user.employee.id
+                enrichment_log_entry.animal_id = form_data['animal']
+                enrichment_log_entry.enrichment_item_id = form_data['enrichment_item']
+                enrichment_log_entry.date = form_data['date']
+                enrichment_log_entry.note = form_data['note']
+
+                enrichment_log_entry.save()
+
+                return redirect(reverse('actnaturalapp:enrichment_log_entries'))
+
+        else:
+        
+            new_enrichment_log_entry = EnrichmentLogEntry.objects.create(
+                employee_id = request.user.employee.id,
+                animal_id = form_data['animal'],
+                enrichment_item_id = form_data['enrichment_item'],
+                date = form_data['date'],
+                note = form_data['note']
+            )
+
+            return redirect(reverse('actnaturalapp:enrichment_log_entries'))
