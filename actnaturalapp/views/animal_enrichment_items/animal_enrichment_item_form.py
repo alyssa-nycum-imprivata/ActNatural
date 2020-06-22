db4b0579c088f7db7527d83ad0af3767ac7825c6
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-06-22 17:29:52 -0500

    commented code for all animal view files and animal enrichment item view files

--- actnaturalapp/views/animal_enrichment_items/animal_enrichment_item_form.py
+++ actnaturalapp/views/animal_enrichment_items/animal_enrichment_item_form.py
@@ -8,14 +8,19 @@ def animal_enrichment_item_form(request, enrichment_item_id):
 
     if request.method == 'GET':
         
+        # grabs all of the animals with a team_id that matches the logged in user's team_id
         all_animals = Animal.objects.filter(team_id=request.user.employee.team_id)
+        # grabs the specific enrichment item the user wants to add animals to
         enrichment_item = EnrichmentItem.objects.get(pk=enrichment_item_id)
+        # grabs all of the animal enrichment item objects associated with the specific enrichment item
         animal_enrichment = AnimalEnrichmentItem.objects.filter(enrichment_item_id=enrichment_item.id)
+        
+        # for every animal in the animal enrichment item objects, append the animal to the approved_animals list
         approved_animals = []
-
         for animal in animal_enrichment:
             approved_animals.append(animal.animal)
 
+        # subtract each animal from the approved_animals list from the all_animals list and store the difference in the unapproved_animals list
         set_difference = set(all_animals) - set(approved_animals)
         unapproved_animals = list(set_difference)
 
@@ -23,7 +28,6 @@ def animal_enrichment_item_form(request, enrichment_item_id):
         context = {
             'enrichment_item': enrichment_item,
             'unapproved_animals': unapproved_animals
-
         }
 
         return render(request, template, context)
@@ -33,14 +37,19 @@ def animal_enrichment_item_form_2(request, animal_id):
 
     if request.method == 'GET':
         
+        # grabs all of the enrichment items that has the same team_id as the logged in user's team_id
         all_enrichment_items = EnrichmentItem.objects.filter(team_id=request.user.employee.team_id)
+        # grabs the specific animal the user wants to add enrichment items to
         animal = Animal.objects.get(pk=animal_id)
+        # grabs all of the animal enrichment item objects associated with the specific animal
         animal_enrichment = AnimalEnrichmentItem.objects.filter(animal_id=animal.id)
-        approved_enrichment = []
 
+        # for every enrichment item in the animal enrichment item objects, append the enrichment item to the approved_enrichment list
+        approved_enrichment = []
         for item in animal_enrichment:
             approved_enrichment.append(item.enrichment_item)
 
+        # subtract each enrichment item from the approved_enrichment list from the all_enrichment_items list and store the difference in the unapproved_enrichment list
         set_difference = set(all_enrichment_items) - set(approved_enrichment)
         unapproved_enrichment = list(set_difference)
 
--- actnaturalapp/views/animals/animal_list.py
+++ actnaturalapp/views/animals/animal_list.py
@@ -8,8 +8,11 @@ def animal_list(request, species_id=None):
     
     if request.method == 'GET':
 
+        # grabs the logged in user's employee object
         employee = Employee.objects.get(pk=request.user.employee.id)
+        # grabs all of the animals that have the same team_id as the logged in user's team_id
         animals = Animal.objects.filter(team_id=request.user.employee.team_id)
+        # grabs all of the species that have the same team_id as the logged in user's team_id
         species = Species.objects.filter(team_id=request.user.employee.team_id)
 
         template = 'animals/animal_list.html'
@@ -28,6 +31,8 @@ def animal_list(request, species_id=None):
 
         if ('age' in form_data):
 
+            # if the 'age' property is included in the form data, post a new animal object from the data typed into the form and then redirect to the new animal's details page
+
             new_animal = Animal.objects.create(
                 team_id = request.user.employee.team_id,
                 species_id = form_data['species'],
@@ -37,23 +42,29 @@ def animal_list(request, species_id=None):
                 weight = form_data['weight'],
                 image = form_files['image']
             )
-
+            
             return redirect(reverse('actnaturalapp:animal', args=[new_animal.id]))
 
         elif (
             "actual_method" in form_data
         ):
 
+            # checks for a hidden input field with name="actual_method"
+
+            # grabs the specific species object
             species = Species.objects.get(pk=species_id)
 
-
             if (form_data["actual_method"] == "DELETE"):
+
+                # if the value of the hidden input is "DELETE", delete the specific species and redirect to the animals list
 
                 species.delete()
 
                 return redirect(reverse('actnaturalapp:animals'))
 
             elif (form_data["actual_method"] == "PUT"):
+
+                # if the value of the hidden input is "PUT", edit the specific species from the data typed into the form and redirect to the animals list
 
                 species.team_id = request.user.employee.team_id
                 species.name = form_data['name']
@@ -63,6 +74,8 @@ def animal_list(request, species_id=None):
                 return redirect(reverse('actnaturalapp:animals'))
 
         else:
+
+            # if 'age' is not included in the form data and neither is a hidden input field, then create a new species from the data typed into the form and then redirect to the animal form
 
             new_species = Species.objects.create(
                 team_id = request.user.employee.team_id,
