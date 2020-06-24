cce0a149e2573910c2c6847c018031b6e012ea6c
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-06-24 09:42:18 -0500

    approval step is functioning for vets

--- actnaturalapp/views/animal_enrichment_items/animal_enrichment_item_pending.py
+++ actnaturalapp/views/animal_enrichment_items/animal_enrichment_item_pending.py
@@ -79,12 +79,19 @@ def animal_enrichment_items_pending_vet_approval(request):
     species = Species.objects.all()
     enrichment_items = EnrichmentItem.objects.all()
     enrichment_types = EnrichmentType.objects.all()
-    teams = Team.objects.all()
 
     unapproved_animal_enrichment_items = []
     for item in all_animal_enrichment_items:
         if item.is_vet_approved == False:
             unapproved_animal_enrichment_items.append(item)
+
+    teams = []
+    for item in unapproved_animal_enrichment_items:
+        team = item.animal.team.name
+        teams.append(team)
+
+    teams = set(teams)
+    teams = list(teams)
 
     if request.method == 'GET':
 
@@ -104,3 +111,24 @@ def animal_enrichment_items_pending_vet_approval(request):
 
         else:
             return redirect(reverse('actnaturalapp:enrichment_items'))
+
+    if request.method == 'POST':
+
+        form_data = request.POST
+
+        if (
+            "actual_method" in form_data
+            and form_data["actual_method"] == "PUT"
+        ):
+
+            selected_items = form_data.getlist('items')
+
+            for item in selected_items:
+                animal_enrichment_item = AnimalEnrichmentItem.objects.get(pk=item)
+                animal_enrichment_item.animal_id = animal_enrichment_item.animal_id
+                animal_enrichment_item.enrichment_item_id = animal_enrichment_item.enrichment_item_id
+                animal_enrichment_item.is_manager_approved = animal_enrichment_item.is_manager_approved
+                animal_enrichment_item.is_vet_approved = True
+                animal_enrichment_item.save()
+
+            return redirect(reverse('actnaturalapp:animal_enrichment_items_pending_vet_approval'))
