211f42c1d7d50d472adc5fb51ad56da78373d6ee
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-06-24 13:59:34 -0500

    created mobile nav bar

--- actnaturalapp/static/scripts/base.js
+++ actnaturalapp/static/scripts/base.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.FormSelect.init(elems, options);
 });
 
-
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+});
 
 
