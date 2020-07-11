ed1c718700a1951b492502f1228e280fc47225d2
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-07-11 16:49:33 -0500

    converted species form from using ORM to using raw SQL

--- actnaturalapp/views/species/species_form.py
+++ actnaturalapp/views/species/species_form.py
@@ -1,7 +1,21 @@
+import sqlite3
 from django.shortcuts import render, redirect, reverse
+from ..connection import Connection
 from django.contrib.auth.decorators import login_required
 from actnaturalapp.models import Species, Team, Employee
 
+
+def create_team(cursor, row):
+    
+    """Creates a team instance"""
+
+    _row = sqlite3.Row(cursor, row)
+
+    team = Team()
+    team.id = _row['id']
+    team.name = _row['name']
+
+    return team
 
 @login_required
 def species_form(request):
@@ -9,8 +23,21 @@ def species_form(request):
 
         """GETS the logged in user's team object to save in the add new species form."""
 
-        team = Team.objects.get(pk=request.user.employee.team_id)
+        with sqlite3.connect(Connection.db_path) as conn:
+            conn.row_factory = create_team
+            db_cursor = conn.cursor()
+            user_team_id = request.user.employee.team_id
+            print('LOOK HERE', user_team_id)
 
+            db_cursor.execute("""
+            SELECT *
+            FROM actnaturalapp_team t
+            WHERE t.id = ?
+            """, (user_team_id,))
+            
+            team = db_cursor.fetchone()
+            print('LOOK HERE', team)
+            
         template = 'species/species_form.html'
         context = {
             "team": team
